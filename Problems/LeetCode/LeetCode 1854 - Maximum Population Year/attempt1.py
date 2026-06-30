c9b858fbef7c28bc4e71d07a303671a1b9b9a035
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-06-30 19:37:25 +0530

    attempt: solved LC 1854

---
+++ Problems/LeetCode/LeetCode 1854 - Maximum Population Year/attempt1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        times = []
+        maxforyears = []
+        for i in range (len(logs)):
+            times.append([logs[i][0], 'b'])
+            times.append([logs[i][1], 'd'])
+
+        times.sort(key=lambda x: (x[0], x[1]))
+        maximum = 0
+        count = 0
+        i = 0
+        while i in range (len(times)):
+            if times[i][1] == 'b':
+                count += 1
+            else:
+                count -= 1
+                
+            maximum = max(maximum, count) 
+            maxforyears.append([times[i][0], count])  
+            i += 1
+        earliest = min([x for x in maxforyears if x[1] == maximum])    
+        return earliest[0]         
+
+        
+        
